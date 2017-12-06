0211e325962c6c5f74291d30907d95ef89e3affa
Afonso Trêpa Cunha <afonsotrepa@gmail.com>
2017-12-06 15:42:42 +0000

    Added x86-64-gcc

---
+++ x86_64-elf-4.9.1-Linux-x86_64/lib/gcc/x86_64-elf/4.9.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.9.1/configure --target=x86_64-elf --prefix=/home/geist/svn/toolchains/x86_64-elf-4.9.1-Linux-x86_64 --enable-languages=c,c++ --disable-werror";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
